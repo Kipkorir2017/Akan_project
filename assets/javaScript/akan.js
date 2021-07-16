7e08380274a1d68729c4d439bbf3029934527698
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-16 13:37:19 +0300

    add a footer to the index,html file, some css style and add javascript function

---
+++ assets/javaScript/akan.js
@@ -0,0 +1,18 @@
+function myFunction(){
+    // declaring Variables
+    var CC =parseInt(document.getElementById("century").Value);
+    var YY =parseInt(document.getElementById("year").Value);
+    var MM=parseInt(document.getElementById("month").Value);
+    var DD =parseInt(document.getElementById("day").Value);
+    // declaring variable for the formulae to calculate day
+    var day= (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7;
+
+        // declaring variables for the names
+    var male =["Kwasi","Kwadwo","Kwabena","Kwaku","yaw","Kofi","Kwame"];
+    var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+    // using flow control i.e if and else statements
+    if ()
+
+
+}
