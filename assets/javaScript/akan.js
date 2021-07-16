8bce47894b541a71ada6958c5033777b0dc938aa
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-16 23:20:26 +0300

    change the source path for the js file, also rectified footer section

--- assets/javaScript/akan.js
+++ assets/javaScript/akan.js
@@ -8,45 +8,41 @@ function myFunction(){
     var day= parseInt((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7);
 
         // declaring variables for the gender and names 
-    var gender=document.getElementById("male").value;
-    var gender=document.getElementById("female").value;
+   
+    var genders = document.getElementsByName("gender").checked;
     var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"]
     var male =["Kwasi","Kwadwo","Kwabena","Kwaku","yaw","Kofi","Kwame"];
     var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    
-    
-    
-    
-
+   
     // using flow control i.e if and else statements
-    if (day>1){
-        alert(male[0]+" on: "+days[0])
+    if (day>=0){
+        alert("Your Akan Name is : "+male[0]+", born on: "+days[0])
 
     }
-    else if (day<3 && gender==="male"){
-        alert(male[1],days[1])
+    // else if (day<3 && gender==="male"){
+    //     alert(male[1],days[1])
 
-    }
-    else if (day==2 && gender==="male"){
-        alert(male[2],days[2])
+    // }
+    // else if (day==2 && gender==="male"){
+    //     alert(male[2],days[2])
 
-    }
-    else if (day==3 && gender==="male"){
-        alert(male[3],days[3])
+    // }
+    // else if (day==3 && gender==="male"){
+    //     alert(male[3],days[3])
 
-     }
-     else if (day==4 && gender==="male"){
-        alert(male[4],days[4])
+    //  }
+    //  else if (day==4 && gender==="male"){
+    //     alert(male[4],days[4])
 
-    }
-     else if (day==5 && gender==="male"){
-         alert(male[5],days[5])
+    // }
+    //  else if (day==5 && gender==="male"){
+    //      alert(male[5],days[5])
 
-     }
-     else if (day==5 && gender==="male"){
-        alert(male[5],days[5])
+    //  }
+    //  else if (day==5 && gender==="male"){
+    //     alert(male[5],days[5])
 
-     }
+    //  }
     else{
         alert("Wrong input")
     }
