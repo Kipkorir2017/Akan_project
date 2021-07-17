460355a0ee8f81e03d965fdafb598e7c4579ccde
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-17 13:41:12 +0300

    change the select option and add js validation for the gender

--- assets/javaScript/akan.js
+++ assets/javaScript/akan.js
@@ -6,12 +6,14 @@ function akan(){
     var DD =document.getElementById("day").value;
     
     // declaring variable for the formulae to calculate day
-    var day=(((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7;
-  
+    var day=parseInt((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7);
+
+    // var day =new Date(+CC+ ","+YY+","+MM+","+DD);
+    // var dayOfTheWeek = day.getDay();
 
         // declaring variables for the gender and names 
    
-    var genders = document.getElementsByName("gender").checked=true;
+   
     
    
    
@@ -19,9 +21,66 @@ function akan(){
     var male =["Kwasi","Kwadwo","Kwabena","Kwaku","yaw","Kofi","Kwame"];
     var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     
-    if (day>=0){
-        alert("Your Akan Name is : "+male[0]+", born on: "+days[0])
+    if (CC.length<2 || CC.length>2){
+        alert("invalid century");
+        return false;
     }
+    else if(YY.length<2 || YY.length>2 ){
+        alert("invalid year")
+        return false;
+    }
+    else if(MM<1 || MM >12){
+        alert("invalid month");
+        return false;
+    }
+    else if (DD<1 || DD >31){
+        alert("Invalid Date")
+    }
+    else if (MM == 4 || MM==6  || MM == 9 || MM == 11) { 
+        alert("Please enter a valid date!");
+    }
+    else if(MM==2 || MM>29){
+        alert("please Enter a Valid date")
+    }
+
+   // validating day and gender
+    var genderMale = document.getElementById("gents").checked;
+    var genderFemale=document.getElementById("ladies").checked;
+    
+     if(day ==0 && genderMale===true){
+        alert("Name: "+male[0])
+        return false
+    }
+    else if(day==1 && genderMale===true){
+        alert("Name: "+male[1]+"born on"+days[1])
+        return false;
+    }
+    else if(day ==2 && genderMale===true){
+        alert("Name: "+male[2]+"born on"+days[1])
+        return false;
+    }
+    else if(day ==3 && genderMale===true){
+        alert("Name: "+male[3]+"born on"+days[1])
+        return false;
+    }
+    else if(day ==4 && genderMale===true){
+        alert("Name: "+male[4]+"born on"+days[4])
+        return false;
+    }
+    else if(day ==5 && genderMale==true){
+        alert("Name: "+male[5]+"born on"+days[5])
+        return false;
+    }
+    else if(day==6 && genderMale==true){
+        alert("Name: "+male[6]+"born on"+days[6])
+        return false;
+    }
+    else{
+        alert("You did not select your Gender")
+    }
+   
+   
+
        
 
     
