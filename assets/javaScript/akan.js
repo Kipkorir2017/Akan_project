b4bf1565a18af75bcc6709ae803e7b221270204b
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-18 14:19:41 +0300

    add a js function to change year and century to string

--- assets/javaScript/akan.js
+++ assets/javaScript/akan.js
@@ -4,6 +4,12 @@ function akan(){
     var YY =document.getElementById("year").value;
     var MM=document.getElementById("month").value;
     var DD =document.getElementById("day").value;
+
+    var centuryString = CC.toString();
+    var yearString = YY.toString();    
+    var fullyear =  parseInt(centuryString+yearString)
+
+    
     
     // declaring variable for the formulae to calculate day
     var day=parseInt((((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD )%7);
@@ -32,13 +38,27 @@ function akan(){
     }
     else if (DD<1 || DD >31){
         alert("Invalid Date")
+        return false;
     }
     else if ((MM == 4 || MM==6  || MM == 9 || MM == 11) && DD<1 || DD >30) { 
         alert("Please enter a valid date!");
+        return false;
     }
     else if(MM==2 && DD>=30){
         alert("please Enter a Valid date, should not be more than 29th");
+        return false;
     }
+    else if((MM==2 && DD>28) && (fullyear %4 !=0)){
+        alert("Feb has no more than 28 days, this is not a leap year")
+        return false;
+
+    }
+    else if((MM==2 && DD>29) && (fullyear %4 ==0)){
+        alert("Feb has no more than 29 days, this is  a leap year");
+        return false;
+
+    }
+    
     
    // validating day and gender
     var genderMale = document.getElementById("gents").checked;
@@ -47,61 +67,62 @@ function akan(){
     // validating male inputs
      if(day ==0 && genderMale===true){
         alert("Name: "+male[0]+", born on :"+days[0])
-        return false
+        return false;
+       
     }
     else if(day==1 && genderMale===true){
         alert("Name: "+male[1]+", born on :"+days[1])
-        return false;
+        
     }
     else if(day ==2 && genderMale===true){
         alert("Name: "+male[2]+", born on: "+days[1])
-        return false;
+       
     }
     else if(day ==3 && genderMale===true){
         alert("Name: "+male[3]+", born on: "+days[1])
-        return false;
+      
     }
     else if(day ==4 && genderMale===true){
         alert("Name: "+male[4]+", born on: "+days[4])
-        return false;
+        
     }
     else if(day ==5 && genderMale==true){
         alert("Name: "+male[5]+", born on: "+days[5])
-        return false;
+        
     }
     else if(day==6 && genderMale==true){
         alert("Name: "+male[6]+", born on: "+days[6])
-        return false;
+        
     }
 
     // validating females inputs
     else if(day==0 && genderFemale==true){
         alert("Name: "+female[0]+", born on: "+days[0])
-        return false;
+       
     }
     else if(day==1 && genderFemale==true){
         alert("Name: "+female[1]+", born on: "+days[1])
-        return false;
+        
     }
     else if(day==2 && genderFemale==true){
         alert("Name: "+female[2]+", born on: "+days[2])
-        return false;
+       
     }
     else if(day==3 && genderFemale==true){
         alert("Name: "+female[3]+", born on: "+days[3])
-        return false;
+        
     }
     else if(day==4 && genderFemale==true){
         alert("Name: "+female[4]+", born on: "+days[4])
-        return false;
+        
     }
     else if(day==5 && genderFemale==true){
         alert("Name: "+female[5]+", born on: "+days[5])
-        return false;
+        
     }
     else if(day==6 && genderFemale==true){
         alert("Name: "+female[6]+", born on: "+days[6])
-        return false;
+       
     }
     else{
         alert("You did not select your Gender")
